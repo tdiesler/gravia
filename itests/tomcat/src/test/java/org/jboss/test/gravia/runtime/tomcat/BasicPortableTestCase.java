f559c391545a535539f99870af256db5d2c11db0
Thomas Diesler <thomas.diesler@jboss.com>
2013-10-02 21:48:06 +0200

    First successful run on all three target containers

---
+++ itests/tomcat/src/test/java/org/jboss/test/gravia/runtime/tomcat/BasicPortableTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.gravia.runtime.tomcat;
+
+import java.io.InputStream;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.gravia.resource.ManifestBuilder;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.gravia.runtime.BasicPortableTest;
+import org.junit.runner.RunWith;
+
+/**
+ * Test webapp deployemnts
+ *
+ * @author thomas.diesler@jboss.com
+ * @since 01-Oct-2013
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class BasicPortableTestCase extends BasicPortableTest {
+
+    @Deployment
+    public static WebArchive deployment() {
+        final WebArchive archive = BasicPortableTest.deployment();
+        archive.setManifest(new Asset() {
+            @Override
+            public InputStream openStream() {
+                ManifestBuilder builder = new ManifestBuilder();
+                builder.addIdentityCapability(archive.getName(), "1.0.0");
+                builder.addManifestHeader("Dependencies", "org.jboss.gravia");
+                return builder.openStream();
+            }
+        });
+        return archive;
+    }
+}
