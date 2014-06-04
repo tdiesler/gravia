c90b6667a18066a16c1302d49f76b416f8a25bcc
Thomas Diesler <thomas.diesler@jboss.com>
2014-06-04 21:33:34 +0200

    Push process management API down to fabric8

--- runtime/api/src/main/java/org/jboss/gravia/runtime/spi/RuntimePropertiesProvider.java
+++ runtime/api/src/main/java/org/jboss/gravia/runtime/spi/RuntimePropertiesProvider.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@
 package org.jboss.gravia.runtime.spi;
 
 import org.jboss.gravia.runtime.Runtime;
+import org.jboss.gravia.runtime.RuntimeLocator;
 
 /**
  * A properties provider that delegates to the runtime.
@@ -32,6 +33,10 @@ import org.jboss.gravia.runtime.Runtime;
 public class RuntimePropertiesProvider implements PropertiesProvider {
 
     private final Runtime runtime;
+
+    public RuntimePropertiesProvider() {
+        this.runtime = RuntimeLocator.getRequiredRuntime();
+    }
 
     public RuntimePropertiesProvider(Runtime runtime) {
         this.runtime = runtime;
