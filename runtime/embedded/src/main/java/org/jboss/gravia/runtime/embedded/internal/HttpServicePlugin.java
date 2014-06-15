4dca456373793de0b19d49dd9b367a5cc43e2db2
Thomas Diesler <thomas.diesler@jboss.com>
2014-06-15 10:15:49 +0200

    Allow HttpServicePlugin to work in the embedded runtime

--- runtime/embedded/src/main/java/org/jboss/gravia/runtime/embedded/internal/HttpServicePlugin.java
+++ runtime/embedded/src/main/java/org/jboss/gravia/runtime/embedded/internal/HttpServicePlugin.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@
  */
 package org.jboss.gravia.runtime.embedded.internal;
 
+import org.jboss.gravia.runtime.RuntimeType;
 import org.jboss.gravia.runtime.embedded.spi.AbstractRuntimePlugin;
 
 
@@ -32,7 +33,10 @@ public final class HttpServicePlugin extends AbstractRuntimePlugin {
 
     @Override
     public String getBundleActivator() {
-        return "org.apache.felix.http.bridge.internal.BridgeActivator";
+        if (RuntimeType.getRuntimeType() == RuntimeType.TOMCAT || RuntimeType.getRuntimeType() == RuntimeType.WILDFLY) {
+            return "org.apache.felix.http.bridge.internal.BridgeActivator";
+        } else {
+            return "org.apache.felix.http.bundle.internal.CombinedActivator";
+        }
     }
-
 }
