ecbe1fc0c999ecf2030ea1e7e679366326e50823
Thomas Diesler <thomas.diesler@jboss.com>
2015-04-14 10:27:35 +0200

    Make Repository optional in WildFly integration

--- container/wildfly/extension/src/main/java/org/wildfly/extension/gravia/deployment/GraviaServicesProcessor.java
+++ container/wildfly/extension/src/main/java/org/wildfly/extension/gravia/deployment/GraviaServicesProcessor.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,7 +42,6 @@ public class GraviaServicesProcessor implements DeploymentUnitProcessor {
         Resource resource = depUnit.getAttachment(GraviaConstants.RESOURCE_KEY);
         if (resource != null) {
             phaseContext.addDeploymentDependency(GraviaConstants.RUNTIME_SERVICE_NAME, GraviaConstants.RUNTIME_KEY);
-            phaseContext.addDeploymentDependency(GraviaConstants.REPOSITORY_SERVICE_NAME, GraviaConstants.REPOSITORY_KEY);
         }
     }
 
